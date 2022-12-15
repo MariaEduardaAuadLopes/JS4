2928a59bca25cf19a1540d4dade4a29ea6a6f113
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2022-12-14 22:14:10 -0300

    atualização

---
+++ 02/app.js
@@ -0,0 +1,8 @@
+function repeticao(){
+    let nome = document.getElementById("nome").value 
+    let numero = document.getElementById("numero").value 
+
+    document.getElementById("nome").value = frase
+    document.getElementById("numero").value = numero
+    document.getElementById('resposta'). innerHTML = resposta
+}
